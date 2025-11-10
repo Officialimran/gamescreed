ac55d9ebe12d003a2c1fa90ca12bd3df85a4a058
Imran Ahmed <imran.softwindtech@gmail.com>
2025-11-10 18:22:02 +0600

    landing page created

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        encode: ['"Encode Sans"', 'sans-serif'],
+        oxygen: ['Oxygen', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
